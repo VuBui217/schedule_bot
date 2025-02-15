8dc71ef26c8ad32588399a5f45d6ed16acc0a9c5
VuBui217 <vubui217@gmail.com>
2025-02-14 23:24:36 -0500

    Refactor code for handleInteraction

--- bot/handlers/interactionHandler.js
+++ bot/handlers/interactionHandler.js
@@ -16,54 +16,41 @@ import {
 //import { schedule } from "node-cron";
 
 export async function handleInteraction(interaction) {
-  if (interaction.isCommand() && interaction.commandName === "schedule") {
-    await scheduleHandler(interaction);
+  if (interaction.isCommand()) {
+    if (interaction.commandName === "schedule") {
+      await scheduleHandler(interaction);
+    } else if (interaction.commandName === "edit") {
+      await editHandler(interaction);
+    } else if (interaction.commandName === "delete") {
+      await deleteHandler(interaction);
+    }
+    return;
   }
 
-  if (
-    interaction.isChannelSelectMenu() &&
-    interaction.customId === "channelSelect"
-  ) {
-    await handleChannelSelect(interaction);
+  if (interaction.isChannelSelectMenu()) {
+    if (interaction.customId === "channelSelect") {
+      await handleChannelSelect(interaction);
+    }
+    return;
   }
 
-  if (
-    interaction.isModalSubmit() &&
-    interaction.customId.startsWith("scheduleModal_")
-  ) {
-    await handleModalSummit(interaction);
-  }
-  if (interaction.isCommand() && interaction.commandName === "edit") {
-    await editHandler(interaction);
-  }
-
-  if (
-    interaction.isStringSelectMenu() &&
-    interaction.customId === "editScheduleSelect"
-  ) {
-    await handleEditSelect(interaction);
+  if (interaction.isStringSelectMenu()) {
+    if (interaction.customId === "editScheduleSelect") {
+      await handleEditSelect(interaction);
+    } else if (interaction.customId === "deleteScheduleSelect") {
+      await handleDeleteSelect(interaction);
+    }
+    return;
   }
 
-  if (
-    interaction.isModalSubmit() &&
-    interaction.customId.startsWith("editScheduleModal_")
-  ) {
-    await handleEditModalSubmit(interaction);
-  }
-  if (interaction.isCommand() && interaction.commandName === "delete") {
-    await deleteHandler(interaction);
-  }
-  if (
-    interaction.isStringSelectMenu() &&
-    interaction.customId === "deleteScheduleSelect"
-  ) {
-    await handleDeleteSelect(interaction);
-  }
-
-  if (
-    interaction.isModalSubmit() &&
-    interaction.customId.startsWith("deleteConfirmModal_")
-  ) {
-    await handleDeleteModalSubmit(interaction);
+  if (interaction.isModalSubmit()) {
+    if (interaction.customId.startsWith("scheduleModal_")) {
+      await handleModalSummit(interaction);
+    } else if (interaction.customId.startsWith("editScheduleModal_")) {
+      await handleEditModalSubmit(interaction);
+    } else if (interaction.customId.startsWith("deleteConfirmModal_")) {
+      await handleDeleteModalSubmit(interaction);
+    }
+    return;
   }
 }
